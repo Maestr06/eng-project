21697ecb14b56f7d78c90fbedca9d1e99e853c8c
Marceli Koćwin <988marceli@gmail.com>
2022-12-30 22:15:28 +0100

    added models and forms

--- main/account/urls.py
+++ main/account/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 from .views import UserLoginView
 
 urlpatterns = [
     # login / logout urls with Django ootb views
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
 ]
