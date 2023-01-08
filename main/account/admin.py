b1f995f8dac686c800e38ce7fad1fa9b271eb36e
Marceli Koćwin <988marceli@gmail.com>
2023-01-08 17:04:26 +0100

    created more models, JobTechnology, JobSkill, JobOffer

--- main/account/admin.py
+++ main/account/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import JobOffer, JobSkill, JobTechnology
 
 # Register your models here.
+admin.site.register(JobTechnology)
+admin.site.register(JobSkill)
+admin.site.register(JobOffer)
--- main/account/models.py
+++ main/account/models.py
@@ -1,9 +1,29 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext
 
 # Create your models here.
+class JobTechnology(models.Model):
+    tech_title = models.CharField(max_length=40, unique=True, primary_key=True)
 
+    class Meta:
+        verbose_name_plural = 'Job technologies'
 
+    def __str__(self):
+        return self.tech_title
 
-class JobListing(models.Model):
-    listing_title = models.CharField(max_length=100, default='Oferta pracy')
-    listing_description = models.CharField(max_length=250, default='Opis oferty pracy')
+class JobSkill(models.Model):
+    skill_title = models.CharField(max_length=100, unique=True, primary_key=True)
+
+    def __str__(self):
+        return self.skill_title
+
+class JobOffer(models.Model):
+    offer_title = models.CharField(max_length=100, default='Oferta pracy')
+    offer_tech = models.ForeignKey('JobTechnology', on_delete=models.CASCADE)
+    offer_skills = models.ManyToManyField('JobSkill')
+    offer_description = models.CharField(max_length=250, default='Opis oferty pracy')
+    offer_post_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.offer_title
