d91527c64a41ab739920d6910aaf2b9651397c15
Marceli Koćwin <988marceli@gmail.com>
2023-01-08 15:33:08 +0100

    trying new models

--- main/account/models.py
+++ main/account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class JobListing(models.Model):
+    listing_title = models.CharField(max_length=100, default='Oferta pracy')
+    listing_description = models.CharField(max_length=250, default='Opis oferty pracy')
