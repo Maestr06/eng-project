a88d2cbe1fea28a7e1ccf11998165818f47c59ab
Marceli Koćwin <988marceli@gmail.com>
2023-03-11 12:55:27 +0100

    added models and diagrams

--- main/account/models.py
+++ main/account/models.py
@@ -10,6 +10,12 @@ class Company(models.Model):
     employee_count = models.PositiveIntegerField(null=True, blank=True)
     address = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = 'Companies'
+
+    def __str__(self):
+        return self.company_name
+
 class Technology(models.Model):
     tech_title = models.CharField(max_length=40, unique=True)
 
@@ -40,3 +46,26 @@ class Offer(models.Model):
 
     def __str__(self):
         return self.offer_title
+    
+class Application(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
+    offer = models.ForeignKey('Offer', on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100) #combine first and last name
+    app_text = models.CharField(max_length=250)
+    email = models.EmailField(max_length=100)
+    cv = models.BooleanField(default=False)
+    applied_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user + ' ' + self.offer
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
+    message_text = models.CharField(max_length=250)
+    message_sent_time = models.DateTimeField(auto_now_add=True)
+    message_read_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.user + ' ' + self.company
