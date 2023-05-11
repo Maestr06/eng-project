68f21c65abd63c76eeec1e13d66113b2f31abf33
Marceli Koćwin <988marceli@gmail.com>
2023-05-11 22:56:35 +0000

    finally found a solution to saving filters

--- main/account/filters.py
+++ main/account/filters.py
@@ -1,8 +1,24 @@
 import django_filters
-from .models import Offer
+from django_filters import filters
+from .models import Offer, Technology, Skill, Seniority, Location
+from django.db import models
+from django import forms
 
 class PostFilter(django_filters.FilterSet):
-
+    # offer_tech = filters.ModelChoiceFilter(queryset=Technology.objects.all())
+    offer_skills = filters.ModelMultipleChoiceFilter(queryset=Skill.objects.all(), widget=forms.CheckboxSelectMultiple)
+    # offer_seniority = filters.ModelChoiceFilter(queryset=Seniority.objects.all())
+    # offer_location = filters.ModelChoiceFilter(queryset=Location.objects.all())
+    
     class Meta:
         model = Offer
         fields = ['offer_tech', 'offer_skills', 'offer_seniority', 'offer_location']
+
+        # filter_overrides = {
+        #     models.ManyToManyField: {
+        #         'filter_class': filters.ModelMultipleChoiceFilter(widget = forms.CheckboxSelectMultiple),
+        #         'extra': {
+        #             'widget': forms.CheckboxSelectMultiple,
+        #         },
+        #     },
+        # }
